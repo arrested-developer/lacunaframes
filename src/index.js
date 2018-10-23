0b8c5ed926351870a0eab790a8971862894d886d
Michael Watts <hello@michaelwatts.co>
2018-10-23 13:26:13 +0100

    set up webpack, initial project with rotating frame and zooming text

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './style.scss';
+
+// rotate sections to starting points
+window.addEventListener('scroll', () => {
+  const scrollTracker = document.getElementById('scrollTracker');
+  const pageContainer = document.querySelector('.pageContainer');
+  const frame = document.querySelector('#frame');
+  const spinContainers = document.querySelectorAll('.spinContainer');
+  const zoomContainers = document.querySelectorAll('.zoomContainer');
+  const pageHeight = pageContainer.getBoundingClientRect().height;
+  const scrolledDist = scrollTracker.getBoundingClientRect().top;
+  let rotation = (scrolledDist / (pageHeight * 3)) * 270;
+  const setRotation = rotation > 0 ? 0 : rotation < -270 ? -270 : rotation;
+  const zoomFactor = Math.cos((setRotation / 135) * ((3 * Math.PI) / 2));
+  console.log(zoomFactor);
+  zoomContainers.forEach(
+    container => (container.style.transform = `scale(${Math.abs(zoomFactor)})`)
+  );
+  frame.style.transform = `rotate(${setRotation}deg)`;
+  spinContainers.forEach(
+    container => (container.style.transform = `rotate(${setRotation}deg)`)
+  );
+  const showText = id => {
+    document
+      .querySelectorAll('.text-content')
+      .forEach(elem => (elem.style.opacity = 0));
+    document.querySelector(`${id} .text-content`).style.opacity = 1;
+  };
+  if (setRotation < 0 && setRotation > -45) {
+    showText('#one');
+  } else if (setRotation < -45 && setRotation > -135) {
+    showText('#two');
+  } else if (setRotation < -135 && setRotation > -225) {
+    showText('#three');
+  } else if (setRotation < -225) {
+    showText('#four');
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
